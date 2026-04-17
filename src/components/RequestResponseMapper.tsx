da038f98ea75696d7999329183615be1a0e0ef8d
beka-tegene <bekategene177@gmail.com>
2026-04-17 09:01:47 +0300

    fix

--- src/components/RequestResponseMapper.tsx
+++ src/components/RequestResponseMapper.tsx
@@ -2185,7 +2185,7 @@ export function RequestResponseMapper() {
                       className="w-full px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
                       required
                     >
-                      <option value={""} selected defaultChecked>
+                      <option value="" disabled>
                         Select Group
                       </option>
                       <option value={"Utilities & Post Paid"}>
@@ -2214,6 +2214,9 @@ export function RequestResponseMapper() {
                       className="w-full px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
                       required
                     >
+                      <option value="" disabled>
+                        Select Group code
+                      </option>
                       <option value={"Utilities&Post_Paid"}>
                         Utilities&Post_Paid
                       </option>
